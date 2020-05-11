8494467cc102bed53ab460a3382f4c55650fa462
ronald <rgox369@hotmail.com>
2020-05-11 15:11:16 -0600

    first version

---
+++ src/MainClass.java
@@ -0,0 +1,29 @@
+
+/**
+ *
+ * @author ronald
+ */
+
+public class MainClass {
+
+    int a = 1;
+    String b = "2";
+    boolean c = false;
+    double d = 4.4;
+    long e = 44444;
+    char q = 'd';
+
+    public static void main(String[] args) {
+
+    }
+
+    public int hola(String perro, String gol) {
+        int z = perro.compareTo(gol) + 3;
+        return z;
+    }
+
+    public void saludar() {
+        System.out.println("hola a todos");
+    }
+
+}
